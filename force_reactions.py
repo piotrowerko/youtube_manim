965077aeaeaaccb7f6febd448bfb8a8f456a8e75
piotrowerko <piotrowerko@outlook.com>
2023-03-25 21:03:15 +0100

    simpy suported beam fully animated

--- force_reactions.py
+++ force_reactions.py
@@ -6,7 +6,10 @@
 # Tutorial:
 #  https://www.youtube.com/watch?v=KHGoFDB-raE&ab_channel=BrianAmedee
 
+# manim -p force_reactions.py Beam
+
 from manim import *
+import numpy as np
 
 class Beam(Scene):
     def construct(self):
@@ -18,8 +21,11 @@ class Beam(Scene):
         self.wait()
         self.play(GrowFromCenter(beam),run_time=3)
         self.wait()
-        triangle_1 = self._add_supports(x_init1, x_init2)
-        self.play(GrowFromCenter(triangle_1),run_time=3)
+        tria_1, tria_2, slide = self._add_supports(x_init1, x_init2)
+        self.play(GrowFromEdge(tria_1, UP), 
+                  GrowFromEdge(tria_2, UP), 
+                  GrowFromCenter(slide), run_time=3)
+        #self.play(GrowFromCenter(tria_2),run_time=3)
         self.wait()
 
     def _show_beam(self, x1, x2):
@@ -32,15 +38,19 @@ class Beam(Scene):
     def _add_supports(self, x1, x2):
         x_start = np.array([x1, 2, 0])
         x_end = np.array([x2, 2, 0])
-        triangle_1 = Triangle().scale(0.5)
-        triangle_1.move_to(x_start, UP)
-        #triangle_1.scale(0.5)
-
-        return triangle_1   
+        tria_1 = Triangle().scale(0.5)
+        tria_2 = Triangle().scale(0.5)
+        tria_1.move_to(x_start, UP)
+        tria_2.move_to(x_end, UP)
+        vector = np.array([0,-0.9,0])
+        slide = self._show_beam(x_end[0] - 0.5, x_end[0] + 0.5)
+        slide.shift(vector)
+        return tria_1, tria_2, slide
         
 
 def main():
-    pass
+    my_scene = Beam()
+    my_scene.construct()
 
 
 if __name__ == '__main__':
